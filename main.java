03f0f25fdad55ce7e251c6c8374d9ad588adb714
Fuzzbucket14 <159600680+Fuzzbucket14@users.noreply.github.com>
2024-02-13 09:51:24 -0600

    l

--- main.java
+++ main.java
@@ -1,10 +1,13 @@
 
 import javax.money.*;
 
+
+
 public class Main {
 
     public static void main(String[] args){
         CurrencyUnit usd = Monetary.getCurrency("USD");
+
 
         System.out.println(usd);
 
